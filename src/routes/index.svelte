dc27f8d930d6350c7ce63db6d240b627faf15582
blurrrb <17aakashsingh1999@gmail.com>
2022-05-11 12:10:52 +0530

    test ssr

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const url = 'https://jsonplaceholder.typicode.com/posts';
+		const response = await fetch(url);
+
+		return {
+			status: 200,
+			props: {
+				posts: response.ok && (await response.json())
+			}
+		};
+	}
+</script>
+
+<script>
+	export let posts = '';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>{JSON.stringify(posts)}</p>
